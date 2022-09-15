54511550986b379e65c06d3a3a5bc286aa8a2254
Serhii-Kravchenko-2022 <110328675+Serhii-Kravchenko-2022@users.noreply.github.com>
2022-09-15 18:06:52 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     author_email='flyingcircus@example.com',
     license='MIT',
     packages=find_namespace_packages(),
-    entry_points={'console_scripts': ['clean-folder = clean_folder.clean']}
+    entry_points={'console_scripts': ['clean-folder = clean_folder.clean:main']}
 )
